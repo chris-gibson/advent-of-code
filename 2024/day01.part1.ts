59c3d0a1e9937ea8b906c34f2ccb4306e2fb8f4e
Chris Gibson <cdgibson@icloud.com>
2024-12-01 23:17:38 -0600

    Add Deno configuration and initial implementation for day 01 tasks

---
+++ 2024/day01.part1.ts
@@ -0,0 +1,21 @@
+const fileContents = await Deno.readTextFile("./2024/day01.txt");
+const rows = fileContents.split("\n");
+const left: number[] = [];
+const right: number[] = [];
+rows.forEach((row) => {
+  const thing = row.split("   ");
+  left.push(+thing[0]);
+  right.push(+thing[1]);
+});
+
+export const sortedLeft = left.sort((a, b) => a - b);
+export const sortedRight = right.sort((a, b) => a - b);
+
+const value = sortedLeft.reduce((acc, curr, i) => {
+  const currRight = sortedRight[i];
+  const distance = currRight - curr;
+
+  return acc + Math.abs(distance);
+}, 0);
+
+console.log(value);
---
+++ 2024/day01.part2.ts
@@ -0,0 +1,21 @@
+import { sortedLeft, sortedRight } from "./day01.part1.ts";
+
+const instanceCounts = sortedLeft.reduce((acc: Record<number, number>, curr) => {
+  if (acc[curr]) {
+    return acc;
+  }
+
+  const instances = sortedRight.filter((r) => r === curr).length;
+  if (instances === 0) {
+    return acc;
+  }
+  acc[curr] = instances;
+  return acc;
+
+}, {});
+
+const similarity = Object.entries(instanceCounts).reduce((acc, [key, value]) => {
+  const thing = +key * value;
+  return acc + thing;
+}, 0);
+console.log("Similarity:", similarity);
